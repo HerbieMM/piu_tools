309843e66800613917658b989290fb3da6fc6f7b
Herberth Mendes Marra <herberthmm@hotmail.com>
2020-01-26 21:19:29 -0300

    melhorias details

---
+++ piu_tools/Models/ChartUnlockRequirements.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace piu_tools.Models
+{
+    public class ChartUnlockRequirements
+    {
+        public bool Done { get; set; }
+        public string Description { get; set; }
+    }
+}
